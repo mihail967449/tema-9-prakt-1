75f8af02c9355d7111025312d288089a06d6318d
Sided911 <dima.puluchu.05@mail.ru>
2023-03-12 22:09:46 +0200

    1 zadanie

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
+            int[] array = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write($"array[{i}]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"array[{i}]) ={array[i]}");
+            }
+            Console.Read();
         }
     }
 }
